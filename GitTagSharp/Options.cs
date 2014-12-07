e832d95ac2d8e47c6a4fc0698d070908e8ff80f5
Saanch <sl.sanu@gmail.com>
2014-12-07 21:36:20 +0800

    [SS] Initial commit

---
+++ GitTagSharp/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace GitTagSharp
+{
+  public class Options
+  {
+    [Option('r', "read", Required = true, HelpText = "Repository name to be tagged.")]
+    public string Repository { get; set; }
+
+    [Option('l', "localRepoPath", Required = false, DefaultValue = "C:\\temp", HelpText = "Local repository path.")]
+    public string LocalRepoPath { get; set; }
+
+    [Option('u', "userName", Required = true, HelpText = "UserName to connect to the remote repository.")]
+    public string UserName { get; set; }
+
+    [Option('p', "password", Required = true, HelpText = "Password to connect to the remote repository.")]
+    public string Password { get; set; }
+
+    [Option('e', "email", Required = true, HelpText = "Email to connect to the remote repository.")]
+    public string Email { get; set; }
+
+    [Option('b', "branch", Required = true, HelpText = "Branch name of the remote repository to be tagged.")]
+    public string Branch { get; set; }
+
+    [Option('t', "tagName", Required = true, HelpText = "Tag name to be applied to the remote repository.")]
+    public string TagName { get; set; }
+  }
+}
---
+++ GitTagSharp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+
+namespace GitTagSharp
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+      if (!result.Errors.Any())
+      {
+        string url = result.Value.Repository; 
+
+        var brancOption = result.Value.Branch;
+        var branchSepIndex = brancOption.IndexOf('/');
+        if (branchSepIndex > 0)
+        {
+          var credentials = new UsernamePasswordCredentials
+            {
+              Username = result.Value.UserName,
+              Password = result.Value.Password
+            };
+          CredentialsHandler credHandler = (s, fromUrl, types) => credentials;
+
+          var remote = brancOption.Substring(0, branchSepIndex);
+          var branch = brancOption.Substring(branchSepIndex+1, brancOption.Length - branchSepIndex-1);
+
+          var workingDirectory = result.Value.LocalRepoPath;
+
+          var isLocalRepoExist = Repository.Discover(workingDirectory);
+          if (isLocalRepoExist == null)
+          {
+            var cloneOptions = new CloneOptions {CredentialsProvider = credHandler};
+            Repository.Clone(url, workingDirectory, cloneOptions);
+          }
+
+          Repository repo = null;
+          try
+          {
+            var tagName = result.Value.TagName;
+            repo = new Repository(workingDirectory);
+            //repo.Fetch(remote, new FetchOptions(){CredentialsProvider = credHandler});
+            repo.Network.Pull(new Signature(result.Value.UserName,result.Value.Email, new DateTimeOffset()),
+              new PullOptions() { FetchOptions = new FetchOptions() { CredentialsProvider = credHandler } });
+            repo.ApplyTag(tagName);
+            PushChanges(repo, credHandler, remote, branch, tagName);
+            Console.WriteLine("Tagged :{0}", result.Value.TagName);
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine("Error happened {0}", ex.Message);
+          }
+          finally
+          {
+            if (repo != null) repo.Dispose();
+          }
+        }
+      }
+      Console.ReadLine();
+    }
+
+    public static void PushChanges(Repository repository, CredentialsHandler credentialsHandler, string remote, string branch, string tagName)
+    {
+      try
+      {
+        var remotes = repository.Network.Remotes[remote];
+        var options = new PushOptions {CredentialsProvider = credentialsHandler};
+        string pushRefSpec = string.Format("refs/heads/{0}:refs/tags/{1}", branch, tagName);
+        repository.Network.Push(remotes, pushRefSpec, options);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Exception:RepoActions:PushChanges " + e.Message);
+      }
+    }
+  }
+}
